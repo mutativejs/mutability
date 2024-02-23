5ffe32a5353576fcfd8f640e66192df6085af194
unadlib <unadlib@gmail.com>
2024-02-24 01:37:21 +0800

    docs(comment): add comment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { create } from 'mutative';
 import { apply } from './apply';
 
+/**
+ * Transactional updates to the base state with the recipe.
+ */
 export const mutate = <T>(baseState: T, recipe: (state: T) => void) => {
   const [, patches] = create(baseState, recipe, {
     enablePatches: true,
