cf8bc01d1f579524df6e7b99b1b4aaa468dc1895
unadlib <unadlib@gmail.com>
2024-02-24 00:38:40 +0800

    chore(init): init project

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mutability
+
+![Node CI](https://github.com/unadlib/mutability/workflows/Node%20CI/badge.svg)
+[![npm version](https://badge.fury.io/js/mutability.svg)](http://badge.fury.io/js/mutability)
+
 A JavaScript library for transactional mutable updates
+
+## Motivation
+
+## Installation
+
+```sh
+yarn add mutability
+```
+
+## Usage
+
+
---
+++ rollup.config.js
@@ -0,0 +1,64 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const input = './dist/index.js';
+
+export default isProduction
+  ? {
+      input,
+      output: [
+        {
+          format: 'cjs',
+          exports: 'auto',
+          file: 'dist/index.cjs.js',
+          sourcemap: true,
+        },
+        {
+          format: 'umd',
+          name: pkg.name
+            .split('-')
+            .map(([s, ...rest]) => [s.toUpperCase(), ...rest].join(''))
+            .join(''),
+          file: pkg.unpkg,
+          sourcemap: true,
+          globals: {
+            'data-transport': 'DataTransport',
+            'broadcast-channel': 'BroadcastChannel',
+          },
+        },
+      ],
+      plugins: [
+        resolve(),
+        commonjs(),
+        replace({
+          __DEV__: 'false',
+          preventAssignment: true,
+        }),
+        terser(),
+      ],
+      external: ['data-transport', 'broadcast-channel'],
+    }
+  : {
+      input,
+      output: [
+        {
+          format: 'cjs',
+          exports: 'auto',
+          file: 'dist/index.cjs.development.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [
+        resolve(),
+        commonjs(),
+        replace({
+          __DEV__: 'true',
+          preventAssignment: true,
+        }),
+      ],
+      external: ['data-transport', 'broadcast-channel'],
+    };
