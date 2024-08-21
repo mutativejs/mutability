3c3dbf364a018ef22ce0ea400b287c0526f8d491
unadlib <unadlib@gmail.com>
2024-08-22 01:06:22 +0800

    test(test): update testing

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ export { apply } from './apply';
  * Transactional updates to the base state with the recipe.
  */
 export const mutate = <T>(baseState: T, recipe: (state: T) => void) => {
-  const [, patches] = create(baseState, recipe, {
+  const [, patches, inversePatches] = create(baseState, recipe, {
     enablePatches: true,
   });
   apply(baseState, patches);
-  return patches;
+  return { inversePatches, patches };
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { mutate } from '../src';
+import { mutate, apply } from '../src';
 
 test('base - mutate', () => {
   const baseState = {
@@ -6,10 +6,28 @@ test('base - mutate', () => {
       c: 1,
     },
   };
-  mutate(baseState, (draft) => {
+  const { patches, inversePatches } = mutate(baseState, (draft) => {
     draft.a.c = 2;
   });
   expect(baseState).toEqual({ a: { c: 2 } });
+  expect({ patches, inversePatches }).toEqual({
+    patches: [
+      {
+        op: 'replace',
+        path: ['a', 'c'],
+        value: 2,
+      },
+    ],
+    inversePatches: [
+      {
+        op: 'replace',
+        path: ['a', 'c'],
+        value: 1,
+      },
+    ],
+  });
+  apply(baseState, inversePatches);
+  expect(baseState).toEqual({ a: { c: 1 } });
 });
 
 test('base - mutate with error', () => {
